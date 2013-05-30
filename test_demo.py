a9bec43d643e2de45857c17fbe3c4408501beac3
karthik0918 <baskarkarthik@gmail.com>
2013-05-30 16:28:21 +0000

    copied test file

---
+++ test_demo.py
@@ -0,0 +1,25 @@
+import nose
+from autoburst.sample_class import DemoClass
+
+class TestDemo(object):
+    
+    def setUp(self):
+        self.auto_demo = DemoClass()
+    
+    def tearDown(self):
+        pass
+    
+    def test_case1(self):
+        ret_str = self.auto_demo.returnString()
+        print ret_str
+        pass
+    
+    def test_case2(self):
+        ret_num = self.auto_demo.returnNumber()
+        print ret_num
+        pass
+        
+        
+        
+        
+        
